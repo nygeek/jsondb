ce567a05a9cb1dc01008a1142339a00ec3106bb7
Marc Donner <marc.donner@gmail.com>
2024-09-10 11:04:50 -0400

    got the reader part of the loader working.

--- jsondb_loader.py
+++ jsondb_loader.py
@@ -22,19 +22,30 @@ class JDB_Loader:
         self.n_records = 0
         self.jdb = []
 
+
     def __str__(self):
         """Render the loader object readable."""
         _result = "fn:" + self.fn + "\n"
         _result += "n_records:" + self.n_records + "\n"
         for _record in self.jdb:
             _result += json.dumps(_record, indent=2)
+        return _result
+
 
     def load(self):
         """Actually read and parse the JSON documents from the file"""
+        line = self.fd.readline()
+        while line:
+            print(f"line: '{line}'")
+            line = self.fd.readline()	
+
 
 def main():
     """Main - unit test goes here."""
     print("main()")
+    loader = JDB_Loader("sample_1.jdb")
+    loader.load()
+    
 
 if __name__ == "__main__":
     main()
